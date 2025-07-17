3bfdbe09d5bef7330b4c2bb6d197647bf9d1cd0f
noob-2003 <qqjmj00@naver.com>
2025-07-18 00:43:27 +0900

    ver2.9 commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// ⭐️ [수정] BrowserRouter를 Router로 사용하지 않고 직접 가져옵니다.
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import MainPage from './pages/MainPage';
 import PickAndPack1 from './pages/PickAndPack1';
 import PickAndPack2 from './pages/PickAndPack2';
@@ -8,49 +10,59 @@ import WashAndPack2 from './pages/WashAndPack2';
 import MakeAndPack1 from './pages/MakeAndPack1';
 import MakeAndPack2 from './pages/MakeAndPack2';
 import MakeAndPack3 from './pages/MakeAndPack3';
-import MakeAndPack4 from './pages/MakeAndPack4';
 import DashboardPage from './pages/DashboardPage';
 import AdminLogin from './pages/AdminLogin';
 import FileUpload from './pages/FileUpload';
 import DashboardUI from './pages/DashboardUI';
+
+// ✅ 전역 상태 추가 (MembersProvider)
 import { MembersProvider } from './context/MembersContext';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
-    <Router>
+    // ⭐️ [수정] BrowserRouter를 최상위로 이동
+    <BrowserRouter>
+      {/* ⭐️ [수정] MembersProvider를 BrowserRouter 안으로 이동 */}
       <MembersProvider>
         <Routes>
-        <Route path="/" element={<MainPage />} /> 
+          {/* 기본 메인 페이지 */}
+          <Route path="/" element={<MainPage />} /> 
 
-        <Route path="/pick-pack1" element={<PickAndPack1 />} />
-        <Route path="/pick-pack2" element={<PickAndPack2 />} />
+          {/* Pick & Pack */}
+          <Route path="/pick-pack1" element={<PickAndPack1 />} />
+          <Route path="/pick-pack2" element={<PickAndPack2 />} />
 
-        <Route path="/wash-pack1" element={<WashAndPack1 />} />
-        <Route path="/wash-pack2" element={<WashAndPack2 />} />
-        
-        <Route path="/make-pack1" element={<MakeAndPack1 />} />
-        <Route path="/make-pack2" element={<MakeAndPack2 />} />
-        <Route path="/make-pack3" element={<MakeAndPack3 />} />
-        <Route path="/make-pack4" element={<MakeAndPack4 />} />
-
-        <Route path="/admin-login" element={<AdminLogin onLogin={() => setIsAuthenticated(true)}/>} />
+          {/* Wash & Pack */}
+          <Route path="/wash-pack1" element={<WashAndPack1 />} />
+          <Route path="/wash-pack2" element={<WashAndPack2 />} />
           
-        
+          {/* Make & Pack */}
+          <Route path="/make-pack1" element={<MakeAndPack1 />} />
+          <Route path="/make-pack2" element={<MakeAndPack2 />} />
+          <Route path="/make-pack3" element={<MakeAndPack3 />} />
+
+          {/* Admin 로그인 */}
+          <Route path="/admin-login" element={<AdminLogin onLogin={() => setIsAuthenticated(true)} />} />
+          
+          {/* 대시보드 페이지 */}
           <Route path="/dashboard" element={<DashboardPage />} />
-        
+          {/*<Route path="/dashboard" element={isAuthenticated ? <DashboardPage /> : <Navigate to="/admin-login" replace />} />*/}
 
-        {/* <Route path="/dashboard" element={isAuthenticated ? <DashboardPage /> : <Navigate to="/admin-login" replace />}/> */}
+          <Route path="/dashboardUI" element={<DashboardUI />} />
+          {/*<Route path="/dashboardUI" element={isAuthenticated ? <DashboardUI /> : <Navigate to="/admin-login" replace />} />*/}
 
-        <Route path="/dashboardUI" element={<DashboardUI />} />
-        {/* <Route path="/dashboardUI" element={isAuthenticated ? <DashboardUI /> : <Navigate to="/admin-login" replace />}/> */}
-
-        {/* <Route path="/file-upload" element={<FileUpload />} /> */}
-        <Route path="/file-upload" element={isAuthenticated ? <FileUpload /> : <Navigate to="/admin-login" replace />}/>
-       
+          {/* 파일 업로드 */}
+          <Route
+            path="/file-upload"
+            element={
+              isAuthenticated ? <FileUpload /> : <Navigate to="/admin-login" replace />
+            }
+          />
         </Routes>
       </MembersProvider>
-    </Router>
+    </BrowserRouter>
   );
 }
 
